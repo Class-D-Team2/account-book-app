b5ebdb401dde4fedca0799e9479b1ae9d9c86fa0
김민철(MInCheol Kim) <ofin0613@naver.com>
2024-06-13 19:31:34 +0900

    chartLogic-annotation-delete

--- src/stores/chartLogic.js
+++ src/stores/chartLogic.js
@@ -73,38 +73,4 @@ export const useChartStore = defineStore({
       });
     },
   },
-
-  //   async fetchData() {
-  //     try {
-  //       const response = await axios.get('/api/transactions');
-  //       const state = reactive({ transactions: [] });
-  //       if (response.status === 200) {
-  //         state.transactions = response.data;
-  //       } else {
-  //         alert('데이터 조회 실패');
-  //       }
-
-  //       console.log(state.transactions);
-
-  //       const category = ['a', 'b', 'c', 'd'];
-  //       const filtered = state.transactions
-  //         .filter((transaction) => transaction.type === 'expense')
-  //         .filter(
-  //           (transaction) => transaction.date.substring(0, 7) === '2024-06'
-  //         );
-  //       console.log('!!!!!!!!', filtered);
-  //       for (let i = 0; i < 4; i++) {
-  //         let totalAmount = 0;
-  //         filtered
-  //           .filter((transaction) => transaction.category === category[i])
-  //           .forEach((transaction) => {
-  //             totalAmount += transaction.amount;
-  //           });
-  //         this.chartConfig.data.datasets[i].data = data.values;
-  //       }
-  //     } catch (error) {
-  //       console.error('에러발생 :', error);
-  //     }
-  //   },
-  // },
 });
